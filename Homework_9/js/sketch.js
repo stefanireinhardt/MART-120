50d2cf2ad5022d56dc95dc06e9ac86beb0027427
stefanireinhardt <stefanidemars@yahoo.com>
2020-10-15 18:14:54 -0600

    Uploaded Scetch

---
+++ Homework_9/js/sketch.js
@@ -0,0 +1,50 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  triangle(160,135,200,40,240,135);
+  ellipse(170,185,15,80);
+  ellipse(250,155,80,15);
+  ellipse(286,128,15,60);
+  ellipse(286,85,15,35);
+  triangle(150,145,200,200,250,145);
+  ellipse(230,300,15,100);
+  ellipse(175,300,15,100);
+  rect(155,225,95,50);
+  triangle(150,230,200,120,255,230);
+  ellipse(165,350,40,20);
+  ellipse(240,350,40,20);
+  circle(200,50,30);
+  ellipse(200,120,15,40);
+  ellipse(200,90,55,75);
+  line(190,60,228,90);
+  line(190,60,172,90);
+  circle(188,90,10);
+  circle(210,90,10);
+  circle(188,92,5);
+  circle(188,92,2);
+  circle(210,92,5);
+  circle(210,92,2);
+  point(224,82);
+  point(221,79);
+  point(218,76);
+  point(215,73);
+  point(212,70);
+  point(209,67);
+  point(206,64);
+  point(203,61);
+  point(200,58);
+  point(197,55);
+  line(206,89,218,89);
+  line(180,89,192,89);
+  line(190,114,197,118);
+  line(197,118,205,118);
+  line(205,118,210,115);
+  line(197,105,202,105);
+  text('My Portrait', 10,25);
+  textSize(18);
+  text('by Stefani Reinhardt', 225, 390);
+  
+}
